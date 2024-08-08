357f945f84daba671ffcadc31a54323a7808ca22
Diana Arazklycheva <arazklycheva94@mail.ru>
2024-08-08 14:31:06 +0500

    create login, signup, auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { MainComponent } from './views/main/main.component';
+import { AuthForwardGuard } from './core/auth/auth-forward.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      {path: '', component: MainComponent}
-      // loadChildren: () => import('./').then(m => m...)
+      {path: '', component: MainComponent},
+      {path: '', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule), canActivate: [AuthForwardGuard]}
     ]
-  }
+  },
+  {path: 'choice', redirectTo: ''}
 ];
 
 @NgModule({
---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,93 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { LoginResponseType } from 'src/types/login-response.type';
+import { Observable, Subject, tap } from 'rxjs';
+import { UserInfoType } from 'src/types/user-info.type';
+import { LogoutResponseType } from 'src/types/logout-response.type';
+import { SignupResponseType } from 'src/types/signup-response.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public accessTokenKey: string = 'accessToken';
+  private refreshTokenKey: string = 'refreshToken';
+  private userInfoKey: string = 'userInfo';
+
+  public isLogged$: Subject<boolean> = new Subject<boolean>();
+  private isLogged = false;
+
+  constructor(private http: HttpClient) {
+    this.isLogged = !!localStorage.getItem(this.accessTokenKey);
+  }
+
+  signup(name: string, lastName: string, email: string, password: string): Observable<LoginResponseType> {
+    return this.http.post<SignupResponseType>(environment.apiHost + 'signup', {
+      name,
+      lastName,
+      email,
+      password,
+    });
+  }
+
+  login(email: string, password: string): Observable<LoginResponseType> {
+    return this.http.post<LoginResponseType>(environment.apiHost + 'login', {
+      email,
+      password,
+    })
+      .pipe(
+        tap((data: LoginResponseType) => {
+          if (data.fullName && data.userId && data.accessToken && data.refreshToken) {
+            this.setUserInfo({
+              fullName: data.fullName,
+              userId: data.userId
+            });
+            this.setTokens(data.accessToken, data.refreshToken);
+          }
+        })
+      );
+  }
+
+  logout(): Observable<LogoutResponseType> {
+    const refreshToken: string | null = localStorage.getItem(this.refreshTokenKey);
+    return this.http.post<LogoutResponseType>(environment.apiHost + 'logout', { refreshToken });
+  }
+
+  public getLoggedIn(): boolean {
+    return this.isLogged;
+  }
+
+  public setTokens(accessToken: string, refreshToken: string): void {
+    localStorage.setItem(this.accessTokenKey, accessToken);
+    localStorage.setItem(this.refreshTokenKey, refreshToken);
+    this.isLogged = true;
+    this.isLogged$.next(true);
+  }
+
+  public removeTokens(): void {
+    localStorage.removeItem(this.accessTokenKey);
+    localStorage.removeItem(this.refreshTokenKey);
+    this.isLogged = false;
+    this.isLogged$.next(false);
+  }
+
+  public setUserInfo(info: UserInfoType): void {
+    localStorage.setItem(this.userInfoKey, JSON.stringify(info));
+  }
+
+  public removeUserInfo(): void {
+    localStorage.removeItem(this.userInfoKey);
+  }
+
+  public getUserInfo(): UserInfoType | null {
+    const userInfo: string | null = localStorage.getItem(this.userInfoKey);
+    if (userInfo) {
+      return JSON.parse(userInfo);
+    }
+
+    return null;
+  }
+
+}
